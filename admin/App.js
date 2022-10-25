84d8524fd731c48716e45981596e6a6b04a6cd1f
Sol ha <lefebvre.amelie3376@gmail.com>
2022-10-25 14:27:34 +0200

    admin added

--- admin/App.js
+++ admin/App.js
@@ -1,23 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // components
-import HeaderUser from "./components/header_user/HeaderUser";
+import HeaderAdmin from "./components/header_admin/HeaderAdmin";
 
 // pages
-
-import Index from "./pages/index/Index";
 import Login from "./pages/login/Login";
 
 function App() {
 
-  // display differenet admin either header admin or header user 
+  // display different admin either header admin or header user 
 return(
 <BrowserRouter>
-<HeaderUser/>
+<HeaderAdmin />
+
 <Routes>
-  <Route path="/" element={<Index />}/>
+  <Route>
   <Route path="/admin/login" element={<Login/>}/>
-  </Routes>
+  </Route>
+</Routes>
 </BrowserRouter>
   );
 }
