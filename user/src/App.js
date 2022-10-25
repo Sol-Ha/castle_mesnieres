0e78a72d97b1ca8fc56f6d1961a67e927b5ecd6c
Sol ha <lefebvre.amelie3376@gmail.com>
2022-10-25 16:35:36 +0200

    add

--- user/src/App.js
+++ user/src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // components
-import HeaderUser from "./components/header_user/HeaderUser";
+import HeaderUser from "./components/header_admin/HeaderAdmin";
+import FooterUser from "./components/footer_admin/FooterAdmin";
+// import HeaderAdmin from "./components/header_admin/HeaderAdmin";
 
 // pages
 
@@ -9,16 +11,18 @@ import Index from "./pages/index/Index";
 import Login from "./pages/login/Login";
 
 function App() {
+  // display differenet admin either header admin or header user
+  return (
+    <BrowserRouter>
+      <HeaderUser />
 
-  // display differenet admin either header admin or header user 
-return(
-<BrowserRouter>
-<HeaderUser/>
-<Routes>
-  <Route path="/" element={<Index />}/>
-  <Route path="/admin/login" element={<Login/>}/>
-  </Routes>
-</BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/admin/login" element={<Login />} />
+      </Routes>
+
+      <FooterUser />
+    </BrowserRouter>
   );
 }
 
--- user/src/components/header_admin/HeaderAdmin.jsx
+++ user/src/components/header_admin/HeaderAdmin.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React from 'react';
+
+import { Dropdown, DropdownButton, Image } from "react-bootstrap";
+import "./headerAdmin.scss";
+import Image1 from "../../assets/images/logo.avif";
 
 const HeaderAdmin = () => {
   return (
-    <div>HeaderAdmin</div>
+    <Image1 alt="illustration d'une porte" style={{ width: "16vh" }}/>
   )
 }
 
