fe90c3ff64c9fca8d6cf64fff38d7375c643a748
Sol ha <lefebvre.amelie3376@gmail.com>
2022-10-28 12:53:27 +0200

    back nav set

--- user/src/components/card/Card.jsx
+++ user/src/components/card/Card.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./card.scss";
 
 // building card with function
-function Card({ name }) {
+function Card({ name, urlCard }) {
   return (
-    <a href="#" className="card_back">
+    <a href={urlCard} className="card_back">
       <h3>{name}</h3>
       <div className="animation_card"></div>
     </a>
--- user/src/pages/selection_menu/SelectionMenu.jsx
+++ user/src/pages/selection_menu/SelectionMenu.jsx
@@ -3,43 +3,45 @@ import { Image, Container } from "react-bootstrap";
 import ReactLoading from "react-loading";
 import axios from "axios";
 
-import Card from "../../components/card/Card"
+import Card from "../../components/card/Card";
 import "./selectionMenu.scss";
 
 const SelectionMenu = () => {
-
   return (
     <>
-    {/* DISCONNECTING USER */}
+      {/* DISCONNECTING USER */}
       <nav className="nav_SelectionMenu">
         <button>
-          <Image src="../assets/icones/off.svg"
-        alt="icone de deconnexion"/>
+          <Image src="../assets/icones/off.svg" alt="icone de deconnexion" />
           <a href="Login">DECONNEXION</a>
         </button>
       </nav>
 
       <main className="main_SelectionMenu">
-
         <Container fluid>
-          <Image src="../assets/images/paint_brush1.png"
-        alt="icone de deconnexion"/>
+          <Image
+            src="../assets/images/paint_brush1.png"
+            alt="icone de deconnexion"
+          />
 
-        {/* USER: GRAB USER FIRSTNAME */}
-          <p>Bonjour, !<br/>
-          Par ou souhaitez-vous commencer?
+          {/* USER: GRAB USER FIRSTNAME */}
+          <p>
+            Bonjour, !<br />
+            Par ou souhaitez-vous commencer?
           </p>
         </Container>
-        
+
         <Container fluid>
-          <Card
-          name="CREER UN NOUVEL UTILISATEUR"
-          />
+          <Card name="CREER UN NOUVEL UTILISATEUR" />
+          <Card name="EVENEMENTS & PRESSE" />
+          <Card name="NOS PARTENAIRES" />
+          <Card name="EMPLOIS & STAGES" />
+          <Card name="VISITER" />
+          <Card name="HEBERGEMENT" />
+          <Card name="LOUER NOS SALLES" />
+          <Card name="RESTAURATION" />
         </Container>
       </main>
-
-
-
     </>
   );
 };
