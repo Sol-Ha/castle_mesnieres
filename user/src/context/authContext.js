79acf73ab50cddb305e42931c01e1a0eb3bcfae8
tgabor1 <t.gabor@tutanota.com>
2022-11-10 22:27:50 +0100

    _

--- user/src/context/authContext.js
+++ user/src/context/authContext.js
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useReducer } from "react";
 
 const INITIAL_STATE = {
-    user: JSON.parse(localStorage.getItem("user")) || null,
+    admin: JSON.parse(localStorage.getItem("admin")) || null,
     loading: false,
     error: null
 };
@@ -12,28 +12,28 @@ const AuthReducer = (state, action) => {
     switch (action.type) {
         case "LOGIN_START":
             return {
-                user: null,
+                admin: null,
                 loading: true,
                 error: null
             };
 
         case "LOGIN_SUCCESS":
             return {
-                user: action.payload,
+                admin: action.payload,
                 loading: false,
                 error: null
             };
 
         case "LOGIN_FAILURE":
             return {
-                user: null,
+                admin: null,
                 loading: false,
                 error: action.payload
             };
 
         case "LOGOUT":
             return {
-                user: null,
+                admin: null,
                 loading: false,
                 error: null
             };
@@ -47,13 +47,13 @@ export const AuthContextProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AuthReducer, INITIAL_STATE);
 
     useEffect(() => {
-        localStorage.setItem("user", JSON.stringify(state.user));
-    }, [state.user]);
+        localStorage.setItem("admin", JSON.stringify(state.admin));
+    }, [state.admin]);
 
         return (
             <AuthContext.Provider
                 value={{
-                    user: state.user,
+                    admin: state.admin,
                     loading: state.loading,
                     error: state.error,
                     dispatch
