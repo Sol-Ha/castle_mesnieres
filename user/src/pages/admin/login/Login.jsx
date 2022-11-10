20570ca436829d47fdcfa8e0208c7983aa53f12f
tgabor1 <t.gabor@tutanota.com>
2022-11-10 13:56:49 +0100

    modifs

--- user/src/pages/admin/login/Login.jsx
+++ user/src/pages/admin/login/Login.jsx
@@ -25,6 +25,7 @@ const Login = () => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
+
       const res = await axios.post("/admin/login", credentials);
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     } catch (err) {
@@ -74,3 +75,52 @@ const Login = () => {
 };
 
 export default Login;
+
+
+
+
+
+
+// import axios from "axios"
+// import { useContext, useState } from "react"
+// import { AuthContext } from "../../../context/authContext"
+// import "../../../styles/pages.scss";
+
+// const Login = () => {
+//   const [credentials, setCredentials] = useState({
+//     username: undefined,
+//     password: undefined
+//   })
+
+//   const { user, loading, error, dispatch } = useContext(AuthContext)
+
+//   const handleChange = (e) => {
+//     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }))
+//   }
+
+//   const handleClick = async e => {
+//     e.preventDefault()
+//     dispatch({ type: "LOGIN_START" })
+//     try {
+//       const res = await axios.post("/auth/login", credentials)
+//       dispatch({ type: "LOGIN_SUCCESS", payload: res.data })
+//     } catch (err) {
+//       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data })
+//     }
+//   }
+
+//   console.log(user);
+
+//   return (
+//     <div className="login">
+//       <div className="lContainer">
+//         <input type="text" placeholder="username" id="username" />
+//         <input type="password" placeholder="password" id="password" onChange={handleChange} className="lInput" />
+//         <button onClick={handleClick} className="lButton">Login</button>
+//         {error && <span>{error.message}</span>}
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Login
