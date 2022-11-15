41132908f34532426c5ffd97314612ef2c227a62
Sol ha <lefebvre.amelie3376@gmail.com>
2022-11-15 14:45:12 +0100

    vacating extras

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,24 +1,57 @@
 // Define the structure we wish to see in database
 // mongoose define that structure 
 // it needs to be imported to the controller
-import mongoose from "mongoose";
-
-const Schema = mongoose.Schema
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
 
 // passing an object with properties
 // unique to avoid another doublon to be registered if currently in db
 const userSchema = new Schema({
 
-    user_nickName:{
+    user_firstName: {
         type: String,
         required: true,
-        unique: true
     },
-    user_password:{
+    user_lastName: {
         type: String,
         required: true,
     }
-})
+    ,
+    user_userName: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    user_email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    user_password: {
+        type: String,
+        required: true,
+    },
+    user_birthday: {
+        type: Date,
+        required: true,
+    },
+    user_answer: {
+        type: String,
+    },
+    user_question: {
+        type: String,
+        required: true,
+    },
+    user_endContract: {
+        type: Date,
+        required: true,
+    },
+    isSuperadmin: {
+        type: Boolean,
+        default: false,
+    }
+},
+    { timestamps: true })
 
-// exporting through module and calling it a chosen name with it's schema
+// exporting through module and calling it a chosen name for the collection
 module.exports = mongoose.model("User", userSchema)
---
+++ api/routes/RouteUser.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const{
+
+} = require("../controllers/userController")
+
+const router = express.Router()
+
+
+
+module.exports = router;
